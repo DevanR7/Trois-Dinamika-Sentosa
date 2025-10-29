7fa69f7b4d81e7f1d4f36d5679b2ce2756e14ea0
DevanR7 <devanramiroputratjen@gmail.com>
2025-10-29 10:02:16 +0700

    Memperbaiki komentar pada metode store untuk mencerminkan pembuatan record BatchPayment

--- app/Http/Controllers/BatchPaymentController.php
+++ app/Http/Controllers/BatchPaymentController.php
@@ -140,7 +140,7 @@ class BatchPaymentController extends Controller
                 $client->refresh();
             }
 
-            // 2. Buat record t
+            // 2. Buat record BatchPayment
             $metodeBatch = '';
             if ($kreditAkanDigunakan > 0) $metodeBatch .= 'Kredit Klien';
             if ($danaInputAkanDigunakan > 0) {
