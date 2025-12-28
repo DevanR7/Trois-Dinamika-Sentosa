2215fed87e604abbb7d5bbfa95bcb6ac8ba3c524
DevanR7 <devanramiroputratjen@gmail.com>
2025-12-28 09:14:23 +0700

    refactor: Update Vite configuration for improved build options and chunk handling

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,24 @@ export default defineConfig({
                 'resources/css/admin/app.css',
                 'resources/css/client/app.css',
                 'resources/css/pages/auth.css',
-                
                 'resources/js/admin/app.js',
                 'resources/js/client/app.js',
-                // 'resources/js/public/app.js', // JS Public (Nanti)
             ],
             refresh: true,
         }),
     ],
+    build: {
+        // 1. Naikkan limit peringatan ke 2000 kBs (2 MB)
+        chunkSizeWarningLimit: 2000, 
+        
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+    },
 });
